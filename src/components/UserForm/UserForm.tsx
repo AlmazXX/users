8b50d48a5d3d14e9315ead01e9a25afa32613384
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-10 15:57:38 +0600

    Added logic to form Added functions onUserChange, onCheckboxChange, onFormSubmit

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -1,37 +1,88 @@
-import React from "react";
-import { User } from "../../types";
+import React, { useState } from "react";
+import { User, UserMutation } from "../../types";
 
-interface Props {onSubmit: (user: User) => void}
+interface Props {
+  onSubmit: (user: User) => void;
+}
 
-const UserForm: React.FC<Props> = ({onSubmit}) => {
+const UserForm: React.FC<Props> = ({ onSubmit }) => {
+  const [user, setUser] = useState<UserMutation>({
+    name: "",
+    email: "",
+    role: "",
+    status: false,
+  });
+
+  const onUserChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    setUser((prev) => ({ ...prev, [name]: checked }));
+  };
+
+  const onFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit({
+      id: Date.now(),
+      ...user,
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={onFormSubmit}>
       <h4>Add new user</h4>
       <div className="form-group">
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" className="form-control" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-control"
+          value={user.name}
+          onChange={onUserChange}
+        />
       </div>
       <div className="form-group">
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" className="form-control" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="form-control"
+          value={user.email}
+          onChange={onUserChange}
+        />
       </div>
-
       <div className="form-group">
         <label htmlFor="role">Role</label>
-        <select name="role" id="role" className="form-control">
-          <option value="user"></option>
-          <option value="editor"></option>
-          <option value="admin"></option>
+        <select
+          name="role"
+          id="role"
+          className="form-control"
+          value={user.role}
+          onChange={onUserChange}
+        >
+          <option disabled value="">
+            Please select a value!
+          </option>
+          <option>User</option>
+          <option>Editor</option>
+          <option>Admin</option>
         </select>
       </div>
-
       <div className="form-check mb-3">
-        <label htmlFor="status" className="form-check-label">Status</label>
+        <label htmlFor="status" className="form-check-label">
+          Status
+        </label>
         <input
           type="checkbox"
           name="status"
           id="status"
           className="form-check-input"
+          onChange={onCheckboxChange}
         />
       </div>
       <button type="submit" className="btn btn-primary">
