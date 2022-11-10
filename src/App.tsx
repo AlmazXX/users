0b572f39a19edc619c4b535dc7577bf964478646
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-10 11:04:21 +0600

    Added UserForm, Users comps to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import Toolbar from './components/Toolbar/Toolbar';
+import React from "react";
+import Toolbar from "./components/Toolbar/Toolbar";
+import UserForm from "./components/UserForm/UserForm";
+import Users from "./components/Users/Users";
 
 function App() {
   return (
     <React.Fragment>
       <header>
-        <Toolbar/>
+        <Toolbar />
       </header>
-      <main></main>
+      <main className="container-fluid">
+        <div className="row mt-2">
+          <div className="col-4">
+            <UserForm />
+          </div>
+          <div className="col-4">
+            <Users />
+          </div>
+        </div>
+      </main>
     </React.Fragment>
   );
 }
