b4a5e41794bc2123803fb0ef64b5207fdf237c46
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2022-11-10 11:35:01 +0600

    Created state users

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Toolbar from "./components/Toolbar/Toolbar";
 import UserForm from "./components/UserForm/UserForm";
 import Users from "./components/Users/Users";
+import { User } from "./types";
 
 function App() {
+  const [users, setUsers] = useState<User[]>([])
+
   return (
     <React.Fragment>
       <header>
